39c33129e8ea105cf6f5340fd2b6ab8ea21112c1
Swapnil Dubal <sdubal@deloitte.com>
2018-04-23 18:28:42 +0530

    added gitignore file

--- server/config/production.js
+++ server/config/production.js
@@ -2,6 +2,6 @@ module.exports = {
     // disbable logging for production
     logging: false,
     db: {
-      url: process.env.MONGODB_URI || 'mongodb://localhost/mechanic'
+      url: 'mongodb://swap1789:zxcvzxcv1@ds014578.mlab.com:14578/mechanic'
     }
   };
